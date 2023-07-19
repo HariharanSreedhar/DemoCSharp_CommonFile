996fb969c59c4023a6f59b513e8d03771b823404
Hariharan Sreedhar <hariharan.sreedhar@solitontech.com>
2023-07-19 10:51:47 +0530

    Added project file

---
+++ Calculator_Demo/Calculator_Demo/Calculator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_Demo
+{
+    /// <summary>
+    /// Calculaator class to peform arithmetic operations
+    /// </summary>
+    public class Calculator
+    {
+        /// <summary>
+        /// Method to perform addtion operation
+        /// </summary>
+        /// <param name="x">input 1</param>
+        /// <param name="y">input 2</param>
+        /// <returns>addition of 2 inputs</returns>
+        public float Addition(float x, float y)
+        { 
+            return x + y;
+        }
+
+        /// <summary>
+        /// Method to perform subtraction operation
+        /// </summary>
+        /// <param name="x">input 1</param>
+        /// <param name="y">input 2</param>
+        /// <returns>Subtraction of 2 inputs</returns>
+        public float Subtraction(float x, float y)
+        {
+            return x - y;
+        }
+
+        /// <summary>
+        /// Method to perform multiplication operation
+        /// </summary>
+        /// <param name="x">input 1</param>
+        /// <param name="y">input 2</param>
+        /// <returns>Mulitiplication of 2 inputs</returns>
+        public float Multiplication(float x, float y)
+        {
+            return x * y;
+        }
+
+        /// <summary>
+        /// Method to perform division operation
+        /// </summary>
+        /// <param name="x">input 1</param>
+        /// <param name="y">input 2</param>
+        /// <returns>Division of 2 inputs</returns>
+        public float Division(float x, float y)
+        {
+            return x / y;
+        }
+    }
+}
---
+++ Calculator_Demo/Calculator_Demo/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Calculator_Demo
+{
+    public class Program
+    {
+        // Main method to call calculator functions
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            // Creating new calculator object
+            Calculator calci= new Calculator();
+            Console.WriteLine(calci.Addition(5, 5));
+            Console.WriteLine(calci.Subtraction(5, 5));
+            Console.WriteLine(calci.Multiplication(5, 5));
+            Console.WriteLine(calci.Division(5, 5));
+        }
+    }
+}
